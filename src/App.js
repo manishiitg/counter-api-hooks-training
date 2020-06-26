b45abda88fad2d1e8a123466a4d8da5eeb0d6b86
manish <manish@laptoplenovo>
2020-06-26 14:14:56 +0530

    add hooks

--- src/App.js
+++ src/App.js
@@ -1,17 +1,23 @@
-import React from 'react';
+import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+  let [count, setCount] = useState(1)
+
+  const updateCount = () => {
+    setCount(count + 1)
+  }
   return (
     <div className="App">
       <header className="App-header">
         
-          <h1>1</h1>
+          <h1>{count}</h1>
 
           <br/>
 
-          <button>Count</button>
+          <button onClick={updateCount}>Count</button>
 
       </header>
     </div>
